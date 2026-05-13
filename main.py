59fa9bd8fa7f388f2575c9d7f169dcf1555e4655
Meezan Shaikh <mizansid888@gmail.com>
2026-05-13 20:27:31 +0530

    cors updated for the application

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.routes import router
 from vector_store.faiss_store import FAISSStore
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,3 +9,11 @@ app = FastAPI()
 vector_store = FAISSStore(dimension=1536)  # embedding size for OpenAI model
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tighten later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
